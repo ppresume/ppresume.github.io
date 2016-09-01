17c1016d440c3cdf58ab7b55174af1e2dc1b6bb5
Xiao Hanyu <xiaohanyu1988@gmail.com>
2016-09-01 12:46:32 +0000

    Auto deployed by travis-ci.
    Projects used for this deployment:
    - https://github.com/xiaohanyu/ppresume-web/commit/1aecd5016fc26610a33891afcbbd47fed9aa5506

--- static/javascripts/default.js
+++ static/javascripts/default.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   // put your code here.
   $('.item[data-tab]').tab();
-  $('.ui.sidebar').sidebar('attach events', '.toggle.item', 'toggle')
+  $('.ui.sidebar').sidebar('attach events', '.toggle.item', 'toggle');
+  $('.ui.dropdown').dropdown({on: 'hover'});
+  $('table').addClass('ui striped collapsing table');
 });
